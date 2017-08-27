fa231ef02106a078f4619f83be2a6960ff84e740
raffael rossi <raffael.rossi@fidelize.com.br>
2017-08-27 16:14:57 -0300

    Criado botões, problemas ao mudar estado.

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, AppRegistry, StyleSheet, Button, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {coletar: 0,};
+    }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Um novo começo em uma nova era.</Text>
+        <View style={styles.buttonContainer}>
+            <Button
+                onPress={this.setState({coletar : 1})}
+                title="Iniciar"
+                />
+            <Button
+                onPress={this.setState({coletar : 0})}
+                title="Encerrar"
+                color="#FF2828"
+                />
+        </View>
+        <View style={styles.textContainer}>
+        </View>
       </View>
     );
   }
@@ -18,7 +31,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+      margin: 20,
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+  }
 });
